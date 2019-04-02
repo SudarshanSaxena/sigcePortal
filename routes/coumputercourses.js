9cdedd0c5a154f3f0cc497e700d0c55a9f22760c
sudarshansankey <sudarshan.s@sankeysolutions.com>
2019-04-02 22:11:45 +0530

    ui fixes

--- routes/coumputercourses.js
+++ routes/coumputercourses.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 var M3 = require('../models/ComputerModels/M3');
 var DIS = require('../models/ComputerModels/DIS');
+var DS = require('../models/ComputerModels/DS');
+var DLDA = require('../models/ComputerModels/DLDA');
+var ECCF = require('../models/ComputerModels/ECCF');
 
 router.get('/', (req, res) => {
     res.render('computercourse');
@@ -43,14 +46,74 @@ router.post('/m3', (req, res) => {
 });
 
 router.get('/ds', (req, res) => {
-
+    DS.find({}).sort('-date').exec(function (err, foundPosts) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.render('computer/ds', {
+                posts: foundPosts
+            });
+        }
+    });
 });
+
+router.post('/ds', (req, res) => {
+    var title = req.body.postTitle;
+    var body = req.body.postBody;
+    var author = {
+        id: req.user._id,
+        username: req.user.username,
+        usersName: req.user.usersName
+    }
+    var newPost = {
+        title: title,
+        body: body,
+        author: author
+    };
+    DS.create(newPost, (err, createdPost) => {
+        if (err) {
+            res.send(err);
+        } else {
+            res.redirect('/computercourses/ds');
+        }
+    });
+})
 
 
 
 router.get('/dlda', (req, res) => {
-    res.render('computer/dlda')
+    DLDA.find({}).sort('-date').exec(function (err, foundPosts) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.render('computer/dlda', {
+                posts: foundPosts
+            });
+        }
+    });
 });
+
+router.post('/dlda', (req, res) => {
+    var title = req.body.postTitle;
+    var body = req.body.postBody;
+    var author = {
+        id: req.user._id,
+        username: req.user.username,
+        usersName: req.user.usersName
+    }
+    var newPost = {
+        title: title,
+        body: body,
+        author: author
+    };
+    DLDA.create(newPost, (err, createdPost) => {
+        if (err) {
+            res.send(err);
+        } else {
+            res.redirect('/computercourses/dlda');
+        }
+    });
+})
 
 router.get('/dis', (req, res) => {
     DIS.find({}).sort('-date').exec(function (err, foundPosts) {
@@ -87,8 +150,38 @@ router.post('/dis', (req, res) => {
 })
 
 router.get('/eccf', (req, res) => {
-    res.render('computer/eccf')
+    ECCF.find({}).sort('-date').exec(function (err, foundPosts) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.render('computer/eccf', {
+                posts: foundPosts
+            });
+        }
+    });
 });
+
+router.post('/eccf', (req,res)=>{
+    var title = req.body.postTitle;
+    var body = req.body.postBody;
+    var author = {
+        id: req.user._id,
+        username: req.user.username,
+        usersName: req.user.usersName
+    }
+    var newPost = {
+        title: title,
+        body: body,
+        author: author
+    };
+    ECCF.create(newPost, (err, createdPost) => {
+        if (err) {
+            res.send(err);
+        } else {
+            res.redirect('/computercourses/eccf');
+        }
+    });
+})
 
 
 module.exports = router;

42918ee0eec167005485d24ff41bab5b39d2111d
sudarshansankey <sudarshan.s@sankeysolutions.com>
2019-05-02 10:15:31 +0530

    chat ui

--- routes/chat.js
+++ routes/chat.js
@@ -1,9 +1,15 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var Users = require("../models/User");
 
-router.get('/', (req, res) => {
-    res.render('chat');
+router.get("/", (req, res) => {
+  Users.find({}).sort("usersName").exec((err, foundUsers) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.render("chat",{foundUsers:foundUsers});
+    }
+  });
 });
-
 
 module.exports = router;
